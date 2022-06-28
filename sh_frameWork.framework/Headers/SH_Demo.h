d3334d34f85d3a6dbd65de6bb9a30cc1690842aa
93309810 <1037232639@qq.comcd>
2022-06-28 21:37:40 +0800

    第一次更新

---
+++ sh_frameWork.framework/Headers/SH_Demo.h
@@ -0,0 +1,16 @@
+//
+//  SH_Demo.h
+//  sh_frameWork
+//
+//  Created by 盛恒 on 2022/6/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SH_Demo : NSObject
+- (void)custmoDemo;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sh_frameWork.framework/Headers/sh_frameWork.h
@@ -0,0 +1,18 @@
+//
+//  sh_frameWork.h
+//  sh_frameWork
+//
+//  Created by 盛恒 on 2022/6/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sh_frameWork.
+FOUNDATION_EXPORT double sh_frameWorkVersionNumber;
+
+//! Project version string for sh_frameWork.
+FOUNDATION_EXPORT const unsigned char sh_frameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sh_frameWork/PublicHeader.h>
+#import <sh_frameWork/SH_Demo.h>
+
